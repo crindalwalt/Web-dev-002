7b9e8c77460d84456cbb150629ec1fa94dacb214
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-10-03 10:57:45 +0500

    localstorage second class

--- js/012_localstorage.js
+++ js/012_localstorage.js
@@ -1,30 +1,27 @@
 console.log("wellcome to the localstorage");
+let noteArray = [];
 
-
-let createNote = ()=>{
-    console.log("note has been added");
-
-    let noteElement = document.querySelector("#note").value;
-    console.log( noteElement);
-
-    let priorityElement = document.querySelector("#priority").value;
-    console.log(priorityElement);
-    let timeNow = new Date();
-    let noteObj = {
-        note : noteElement,
-        priority: priorityElement,
-        createdAt : timeNow,
-    }
-    console.log(noteObj);
-}
-
-
-
-
+let handleNote = () => {
+    // grabbing elements
+  let noteElement = document.querySelector("#note").value;
+  let priorityElement = document.querySelector("#priority").value;
+  let timeNow = new Date();
+  // creating an object
+  let noteObj = {
+    note: noteElement,
+    priority: priorityElement,
+    createdAt: timeNow,
+  };
+  // pushing object to an array
+  noteArray.push(noteObj);
+  // convert array to string for saving in localStoage
+  let arrayStringified = JSON.stringify(noteArray);
+  // saving that stringified array in localStorage
+  localStorage.setItem("notes", arrayStringified);
+};
 
 // grabbing elements
-let submitNoteButton  = document.querySelector("#submitNote");
-
+let submitNoteButton = document.querySelector("#submitNote");
 
 // event listeners
-submitNoteButton.addEventListener("click",createNote);
+submitNoteButton.addEventListener("click", handleNote);
